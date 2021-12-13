d7d79a35ecc5fec0201a10b7d4b1c23bc0f65a9f
David Avila <david.avila@briebug.com>
2021-12-13 09:47:41 -0700

    create service and add buisness logic

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -7,10 +7,17 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { ProjectEditComponent } from './projects/project-edit/project-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@resolver-guard/material';
+import { CoreDataModule } from '@resolver-guard/core-data';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, ProjectsComponent, ProjectListComponent, ProjectEditComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreDataModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
