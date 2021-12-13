efdb7d8290d7fb35f2ce07d0505f874bac142251
David Avila <david.avila@briebug.com>
2021-12-13 09:48:13 -0700

    commit unsaved files

---
+++ libs/core-data/src/lib/projects/project.ts
@@ -0,0 +1,13 @@
+export interface Project {
+    id: string,
+    title: string,
+    details: string,
+    importanceLevel: number
+}
+
+export const emptyProject: Project = {
+    id: '0',
+    title: '',
+    details: '',
+    importanceLevel: 0
+  };
--- libs/core-data/src/lib/projects/projects.service.ts
+++ libs/core-data/src/lib/projects/projects.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from './project';
+
+const BASE_URL = 'https://server-30-x-30.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
+  model = 'projects';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUrl(): string {
+    return `${BASE_URL}${this.model}`;
+  }
+
+  getUrlWithId(id: string): string {
+    return `${this.getUrl()}/${id}`
+  }
+
+  getProjects(): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(this.getUrl());
+  }
+
+  getOneProject(id: string): Observable<Project> {
+    return this.httpClient.get<Project>(this.getUrlWithId(id))
+  }
+
+  createProject(project: Project): Observable<Project> {
+    return this.httpClient.post<Project>(this.getUrl(), project);
+  }
+
+  updateProject(project: Project): Observable<Project> {
+    return this.httpClient.patch<Project>(this.getUrlWithId(project.id), project)
+  }
+
+  deleteProject(id: string): Observable<Project> {
+    return this.httpClient.delete<Project>(this.getUrlWithId(id))
+  }
 }
