f9138813cb5ca742a5293e70e2165254768df9b4
David Avila <david.avila@briebug.com>
2021-12-13 09:28:54 -0700

    install material and add components to module for export

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { ProjectEditComponent } from './projects/project-edit/project-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, ProjectsComponent, ProjectListComponent, ProjectEditComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
