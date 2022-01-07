3dd2f61bd0fe1d45d154c40c3cecfb9c06e2694b
David Avila <david.avila@briebug.com>
2022-01-07 11:59:16 -0700

    remove unused file

--- libs/core-state/src/index.ts
+++ libs/core-state/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/+state/projects.facade';
-export * from './lib/+state/projects.models';
 export * from './lib/+state/projects.selectors';
 export * from './lib/+state/projects.reducer';
 export * from './lib/+state/projects.actions';
